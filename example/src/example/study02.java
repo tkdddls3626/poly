3db16d56ce047f9b12212c1502dfc0fdc1ae5f13
data32 <data32@DESKTOP-NRJES5N>
2021-03-17 11:43:14 +0900

    first입력

--- example/src/example/study02.java
+++ example/src/example/study02.java
@@ -3,8 +3,25 @@ package example;
 public class study02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	char a,b,c,d,e;
+	
+	a = 'A';
+	System.out.printf("%c \t",a);
+	System.out.println((int)a);
+	
+	b = a;
+	c = (char)(b+1);
+	System.out.printf("%c \t",b);
+	System.out.printf("%c \n", c);
+	
+	d = 90;
+	System.out.printf("%c \n",d);
+	
+	d = '°¡';
+	e = (char)(d+1);
+	
+	System.out.printf("%d \t",(int) d);
+	System.out.printf("%c \n", e);
+	
 	}
-
 }
