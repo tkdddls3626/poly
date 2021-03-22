ddb3e58afb8b0008b56272a9bf564286266c0872
SangIn <tkddls3626@naver.com>
2021-03-23 00:23:28 +0900

    시분초를 받아 분만 입력되게하기

---
+++ Exam/src/codeup1026.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class codeup1026 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	      String arr = sc.next();
+	        String[] A = arr.split("\\:");
+	        int b = Integer.parseInt(A[1]);
+	 
+	        if(b <= 9)
+	            System.out.printf("%1d", b);
+	        else
+	            System.out.printf("%d", b);
+	    }
+	}
