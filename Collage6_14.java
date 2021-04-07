0fbd472bead0d24a164667661e9dca999e0d4b7e
tkdddls3626 <79887255+tkdddls3626@users.noreply.github.com>
2021-04-07 11:08:06 +0900

    학교수업

---
+++ Collage6_14.java
@@ -0,0 +1,17 @@
+package example;
+
+public class Collage6_14 {
+
+	public static void main(String[] args) {
+		int i, k;
+
+		for (i = 2; i < 10; i++) {
+			for (k = 1; k < 10; k++) {
+			int a = i*k;
+				System.out.println(i+" x "+k+" = "+a);
+			}
+			System.out.println();
+		}
+	}
+
+}
